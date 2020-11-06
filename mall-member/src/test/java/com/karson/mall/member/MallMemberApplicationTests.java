9f2f3e6dbd49ec11417914765e0643aea880d875
karson <308780714@qq.com>
2020-11-06 20:06:27 +0800

    项目初始结构创建

---
+++ mall-member/src/test/java/com/karson/mall/member/MallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karson.mall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
