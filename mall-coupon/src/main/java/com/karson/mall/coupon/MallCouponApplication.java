239331e17825e127c8eec688bf011a843d04011d
karson <308780714@qq.com>
2020-11-07 20:27:24 +0800

    框架搭建

--- mall-coupon/src/main/java/com/karson/mall/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/com/karson/mall/coupon/MallCouponApplication.java
@@ -1,9 +1,13 @@
 package com.karson.mall.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan("com.karson.mall.coupon.dao" )
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MallCouponApplication {
 
     public static void main(String[] args) {
--- mall-member/src/main/java/com/karson/mall/member/MallMemberApplication.java
+++ mall-member/src/main/java/com/karson/mall/member/MallMemberApplication.java
@@ -1,9 +1,15 @@
 package com.karson.mall.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@MapperScan("com.karson.mall.member.dao" )
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.karson.mall.member.feign")
 public class MallMemberApplication {
 
     public static void main(String[] args) {
